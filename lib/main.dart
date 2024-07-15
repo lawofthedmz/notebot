8cf5feeda006b13df934f0b200b76b089869539a
lawofthedmz <murphg04@gmail.com>
2024-07-15 14:39:13 -0500

    got image upload working

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,18 @@ import 'screens/viewprofile.dart';
 import 'screens/create.dart';
 import 'screens/signin.dart';
 import 'screens/home.dart';
-import 'screens/test.dart'; // Import the TestScreen
-
+import 'screens/upload.dart';
 // Firebase imports
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
 
@@ -27,23 +27,14 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         fontFamily: "Montserrat",
       ),
-      // Use StreamBuilder to determine the initial route
-      home: StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            // User is logged in, navigate to HomeScreen
-            return HomeScreen();
-          } else {
-            // User is not logged in, navigate to SignInScreen
-            return SignInScreen();
-          }
-        },
-      ),
+      // Directly navigate to HomeScreen
+      home: HomeScreen(),
       routes: {
         '/signin': (context) => SignInScreen(),
         '/create': (context) => CreateAccountScreen(),
         '/home': (context) => HomeScreen(),
+        '/viewprofile': (context) => ViewProfile(),
+        '/upload': (context) => UploadScreen(),
         // '/editprofile': (context) => EditProfileScreen(),
       },
     );
